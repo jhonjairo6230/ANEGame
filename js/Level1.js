5d709b37da5b8864954b87ed6521640f88bf12b8
Jhon Jairo Ibarra Samboni <jhonjairo6230@gmail.com>
2018-12-24 22:53:42 -0500

    Added platforms for level 1

--- js/Level1.js
+++ js/Level1.js
@@ -1,4 +1,4 @@
-var player;
+var player, platforms;
 RutaEspectral.Level1 = function (game) {};
 RutaEspectral.Level1.prototype = {
     preload: function () {
@@ -18,26 +18,47 @@ RutaEspectral.Level1.prototype = {
     create: function () {
         game.add.tileSprite(0, 0, 1340, 600, 'background');
         game.world.setBounds(0, 0, 1340, 600);
-        game.physics.startSystem(Phaser.Physics.P2JS);
+        game.renderer.roundPixels = true;
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         if (selectedSprite != -1) {
             player = game.add.sprite(100, 100, 'sprite' + selectedSprite);
         } else {
             player = game.add.sprite(100, 100, 'rocket');
         }
-        game.physics.p2.enable(player);
-        player.lastAngle = 0;
-        cursors = game.input.keyboard.createCursorKeys();
+        game.physics.arcade.enable(player);
+        player.body.bounce.y = 0.2;
+        player.body.collideWorldBounds = true;
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = game.add.group();
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
+        //  Now let's create two ledges
+        var ledge = platforms.create(500, 400, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(570, 50, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(700, 250, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(1200, 100, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(900, 100, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(100, game.world.height - 66, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(600, game.world.height - 66, 'platform');
+        ledge.body.immovable = true;
+        ledge = platforms.create(1100, game.world.height - 66, 'platform');
+        ledge.body.immovable = true;
         game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
         // Add the VirtualGamepad plugin to the game
         this.gamepad = this.game.plugins.add(Phaser.Plugin.VirtualGamepad);
-
         // Add a joystick to the game (only one is allowed right now)
         this.joystick = this.gamepad.addJoystick(100, game.world.height - 100, 1.2, 'gamepad');
-
         // Add a button to the game (only one is allowed right now)
         this.button = this.gamepad.addButton(400, 1400, 1.0, 'gamepad');
     },
     update: function () {
+        var hitPlatform = game.physics.arcade.collide(player, platforms);
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
         if (this.joystick.properties.left) {

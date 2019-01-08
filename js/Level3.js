a2acefd24686b49951e8936977bf4a23ec16946d
Jhon Jairo Ibarra Samboni <jhonjairo6230@gmail.com>
2019-01-08 15:28:10 -0500

    Added Assets for Level 3

---
+++ js/Level3.js
@@ -0,0 +1,10 @@
+var player, platforms, elements, cursors, waveCollition;
+var initLVl3 = false;
+var glasses, closeBtn, obstructions, st, planet, bEarth, bEarthH, liveUpGroup, circle, circleT, pltMovement;
+RutaEspectral.Level3 = function (game) {};
+RutaEspectral.Level2.prototype = {
+    preload: function () {},
+    create: function () {},
+    update: function () {},
+    render: function () {}
+}
